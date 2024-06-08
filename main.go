e765991d22bb74c1b9ac5f20cd352938bbca3a91
Vovko <vkouzin@outlook.com>
2024-06-08 17:42:37 -0400

    added recover

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"embed"
 	"io/fs"
-	"log"
+
 	"net/http"
 	"os"
 	"strconv"
@@ -22,6 +22,7 @@ import (
 	"github.com/cufee/aftermath/internal/stats/render/assets"
 	render "github.com/cufee/aftermath/internal/stats/render/common"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -57,21 +58,21 @@ func startSchedulerFromEnvAsync() {
 	// This wargaming client is using a different proxy as it needs a lot higher rps, but can be slow
 	wgClient, err := wargaming.NewClientFromEnv(os.Getenv("WG_CACHE_APP_ID"), os.Getenv("WG_CACHE_APP_RPS"), os.Getenv("WG_CACHE_REQUEST_TIMEOUT_SEC"), os.Getenv("WG_CACHE_PROXY_HOST_LIST"))
 	if err != nil {
-		log.Fatalf("wgClient: wargaming#NewClientFromEnv failed %s", err)
+		log.Fatal().Msgf("wgClient: wargaming#NewClientFromEnv failed %s", err)
 	}
 	dbClient, err := database.NewClient()
 	if err != nil {
-		log.Fatalf("database#NewClient failed %s", err)
+		log.Fatal().Msgf("database#NewClient failed %s", err)
 	}
 	bsClient, err := blitzstars.NewClient(os.Getenv("BLITZ_STARS_API_URL"), time.Second*10)
 	if err != nil {
-		log.Fatalf("failed to init a blitzstars client %s", err)
+		log.Fatal().Msgf("failed to init a blitzstars client %s", err)
 	}
 
 	// Fetch client
 	client, err := fetch.NewMultiSourceClient(wgClient, bsClient, dbClient)
 	if err != nil {
-		log.Fatalf("fetch#NewMultiSourceClient failed %s", err)
+		log.Fatal().Msgf("fetch#NewMultiSourceClient failed %s", err)
 	}
 	coreClient := core.NewClient(client, wgClient, dbClient)
 
@@ -81,8 +82,14 @@ func startSchedulerFromEnvAsync() {
 		taskQueueConcurrency = v
 	}
 	queue := scheduler.NewQueue(coreClient, tasks.DefaultHandlers(), taskQueueConcurrency)
-	queue.StartCronJobsAsync()
 
+	defer func() {
+		if r := recover(); r != nil {
+			log.Error().Interface("error", r).Stack().Msg("scheduler panic")
+		}
+	}()
+
+	queue.StartCronJobsAsync()
 	// Some tasks should run on startup
 	// scheduler.UpdateAveragesWorker(coreClient)()
 	// scheduler.CreateSessionTasksWorker(coreClient, "NA")()
@@ -92,21 +99,21 @@ func coreClientFromEnv() core.Client {
 	// Dependencies
 	wgClient, err := wargaming.NewClientFromEnv(os.Getenv("WG_PRIMARY_APP_ID"), os.Getenv("WG_PRIMARY_APP_RPS"), os.Getenv("WG_REQUEST_TIMEOUT_SEC"), os.Getenv("WG_PROXY_HOST_LIST"))
 	if err != nil {
-		log.Fatalf("wgClient: wargaming#NewClientFromEnv failed %s", err)
+		log.Fatal().Msgf("wgClient: wargaming#NewClientFromEnv failed %s", err)
 	}
 	dbClient, err := database.NewClient()
 	if err != nil {
-		log.Fatalf("database#NewClient failed %s", err)
+		log.Fatal().Msgf("database#NewClient failed %s", err)
 	}
 	bsClient, err := blitzstars.NewClient(os.Getenv("BLITZ_STARS_API_URL"), time.Second*10)
 	if err != nil {
-		log.Fatalf("failed to init a blitzstars client %s", err)
+		log.Fatal().Msgf("failed to init a blitzstars client %s", err)
 	}
 
 	// Fetch client
 	client, err := fetch.NewMultiSourceClient(wgClient, bsClient, dbClient)
 	if err != nil {
-		log.Fatalf("fetch#NewMultiSourceClient failed %s", err)
+		log.Fatal().Msgf("fetch#NewMultiSourceClient failed %s", err)
 	}
 
 	return core.NewClient(client, wgClient, dbClient)
@@ -116,14 +123,14 @@ func loadStaticAssets(static fs.FS) {
 	// Assets for rendering
 	err := assets.LoadAssets(static, "static")
 	if err != nil {
-		log.Fatalf("assets#LoadAssets failed to load assets from static/ embed.FS %s", err)
+		log.Fatal().Msgf("assets#LoadAssets failed to load assets from static/ embed.FS %s", err)
 	}
 	err = render.InitLoadedAssets()
 	if err != nil {
-		log.Fatalf("render#InitLoadedAssets failed %s", err)
+		log.Fatal().Msgf("render#InitLoadedAssets failed %s", err)
 	}
 	err = localization.LoadAssets(static, "static/localization")
 	if err != nil {
-		log.Fatalf("localization#LoadAssets failed %s", err)
+		log.Fatal().Msgf("localization#LoadAssets failed %s", err)
 	}
 }
