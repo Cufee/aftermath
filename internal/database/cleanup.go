102c5d78d08bf0abb0977ce32ded4955f20f662b
Vovko <vkouzin@outlook.com>
2024-06-11 14:09:12 -0400

    added missing code

--- internal/database/cleanup.go
+++ internal/database/cleanup.go
@@ -9,7 +9,19 @@ import (
 
 func (c *client) DeleteExpiredTasks(ctx context.Context, expiration time.Time) error {
 	_, err := c.prisma.CronTask.FindMany(db.CronTask.CreatedAt.Before(expiration)).Delete().Exec(ctx)
-	if err != nil && db.IsErrNotFound(err) {
+	if err != nil && !db.IsErrNotFound(err) {
+		return err
+	}
+	return nil
+}
+
+func (c *client) DeleteExpiredSnapshots(ctx context.Context, expiration time.Time) error {
+	_, err := c.prisma.AccountSnapshot.FindMany(db.AccountSnapshot.CreatedAt.Before(expiration)).Delete().Exec(ctx)
+	if err != nil && !db.IsErrNotFound(err) {
+		return err
+	}
+	_, err = c.prisma.VehicleSnapshot.FindMany(db.VehicleSnapshot.CreatedAt.Before(expiration)).Delete().Exec(ctx)
+	if err != nil && !db.IsErrNotFound(err) {
 		return err
 	}
 	return nil
