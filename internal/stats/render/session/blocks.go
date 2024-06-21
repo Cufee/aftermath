0eab01ac374148ed7705940f5a56b8ef1126c7bc
Vovko <vkouzin@outlook.com>
2024-06-21 13:10:32 -0400

    added indicator icons

--- internal/stats/render/session/blocks.go
+++ internal/stats/render/session/blocks.go
@@ -6,7 +6,10 @@ import (
 	"github.com/cufee/aftermath/internal/stats/frame"
 	prepare "github.com/cufee/aftermath/internal/stats/prepare/common"
 	"github.com/cufee/aftermath/internal/stats/prepare/session"
+	"github.com/cufee/aftermath/internal/stats/render/assets"
 	"github.com/cufee/aftermath/internal/stats/render/common"
+	"github.com/disintegration/imaging"
+	"github.com/fogleman/gg"
 )
 
 func makeSpecialRatingColumn(block prepare.StatsBlock[session.BlockData], width float64) common.Block {
@@ -55,6 +58,10 @@ func makeSpecialRatingColumn(block prepare.StatsBlock[session.BlockData], width 
 	}
 }
 
+func blankBlock(width, height float64) common.Block {
+	return common.NewImageContent(common.Style{Width: width, Height: height}, gg.NewContext(int(width), int(height)).Image())
+}
+
 func vehicleWN8Icon(wn8 frame.Value) common.Block {
 	ratingColors := common.GetWN8Colors(wn8.Float())
 	if wn8.Float() <= 0 {
@@ -63,4 +70,32 @@ func vehicleWN8Icon(wn8 frame.Value) common.Block {
 	}
 	iconTop := common.AftermathLogo(ratingColors.Background, common.SmallLogoOptions())
 	return common.NewImageContent(common.Style{Width: vehicleWN8IconSize, Height: vehicleWN8IconSize}, iconTop)
+}
+
+func vehicleComparisonIcon(session, career frame.Value) common.Block {
+	ctx := gg.NewContext(int(vehicleComparisonIconSize), int(vehicleComparisonIconSize))
+	switch {
+	case session.Float() > career.Float()*1.05:
+		icon, _ := assets.GetLoadedImage("triangle-up-solid")
+		ctx.DrawImage(imaging.Fill(icon, int(vehicleComparisonIconSize), int(vehicleComparisonIconSize), imaging.Center, imaging.Linear), 0, 0)
+		return common.NewImageContent(common.Style{BackgroundColor: color.RGBA{3, 201, 169, 255}}, ctx.Image())
+		// return common.NewImageContent(common.Style{BackgroundColor: common.TextPrimary}, ctx.Image())
+
+	case session.Float() < career.Float()*0.95:
+		icon, _ := assets.GetLoadedImage("triangle-down-solid")
+		ctx.DrawImage(imaging.Fill(icon, int(vehicleComparisonIconSize), int(vehicleComparisonIconSize), imaging.Center, imaging.Linear), 0, 0)
+		return common.NewImageContent(common.Style{BackgroundColor: color.RGBA{231, 130, 141, 255}}, ctx.Image())
+		// return common.NewImageContent(common.Style{BackgroundColor: common.TextPrimary}, ctx.Image())
+
+	default:
+		return common.NewImageContent(common.Style{}, ctx.Image())
+	}
+}
+
+func blockWithVehicleIcon(block common.Block, session, career frame.Value) common.Block {
+	return common.NewBlocksContent(common.Style{}, block, vehicleComparisonIcon(session, career))
+}
+
+func blockShouldHaveCompareIcon(block prepare.StatsBlock[session.BlockData]) bool {
+	return block.Tag != prepare.TagBattles
 }
