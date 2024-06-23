f09d7f2223e04b33df8e5a1d67ce4f1b983c74ea
Vovko <vkouzin@outlook.com>
2024-06-23 18:36:28 -0400

    changed the logo slightly

--- render_test.go
+++ render_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"image/png"
 	"os"
 	"testing"
 	"time"
@@ -10,11 +9,11 @@ import (
 	"github.com/cufee/aftermath/internal/stats"
 	"github.com/cufee/aftermath/internal/stats/render"
 	"github.com/cufee/aftermath/internal/stats/render/assets"
-	"github.com/disintegration/imaging"
-	"github.com/fogleman/gg"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/text/language"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func TestRenderSession(t *testing.T) {
@@ -25,15 +24,6 @@ func TestRenderSession(t *testing.T) {
 	loadStaticAssets(static)
 	coreClient, _ := coreClientsFromEnv()
 	defer coreClient.Database().Disconnect()
-
-	rating, _ := assets.GetLoadedImage("rating-calibration")
-	c := imaging.PasteCenter(gg.NewContext(int(float64(rating.Bounds().Dx())*1.5), int(float64(rating.Bounds().Dy())*1.5)).Image(), rating)
-
-	fc, err := os.Create("tmp/rating-calibration.png")
-	assert.NoError(t, err, "failed to create a file")
-	defer fc.Close()
-
-	png.Encode(fc, c)
 
 	bgImage, ok := assets.GetLoadedImage("bg-default")
 	assert.True(t, ok, "failed to load a background image")
