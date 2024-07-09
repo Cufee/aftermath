66e14915a1992fc8ff7502a1d8a1df49f84b17b1
Vovko <vkouzin@outlook.com>
2024-07-09 16:13:37 -0400

    added logger middleware

--- cmd/core/server/server.go
+++ cmd/core/server/server.go
@@ -10,16 +10,22 @@ type Handler struct {
 	Func http.HandlerFunc
 }
 
-func NewServer(port string, handlers ...Handler) func() {
+func NewServer(port string, handlers []Handler, middleware ...func(http.Handler) http.Handler) func() {
 	mux := http.NewServeMux()
 	for _, h := range handlers {
 		mux.Handle(h.Path, h.Func)
 	}
+
+	var finalHandler http.Handler = mux
+	for i := len(middleware) - 1; i >= 0; i-- { // Reverse order for correct chaining
+		finalHandler = middleware[i](finalHandler)
+	}
+
 	srv := &http.Server{
 		Addr:         ":" + port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  5 * time.Second,
-		Handler:      mux,
+		Handler:      finalHandler,
 	}
 	return func() {
 		panic(srv.ListenAndServe())
