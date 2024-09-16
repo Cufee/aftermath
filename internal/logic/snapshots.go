b216b1f6c6815f9ccc128bb806fa77b1c5fd3c20
Vovko <vkouzin@outlook.com>
2024-09-16 13:24:44 -0400

    added zero checks

--- internal/logic/snapshots.go
+++ internal/logic/snapshots.go
@@ -47,7 +47,7 @@ func filterActiveAccounts(ctx context.Context, dbClient database.Client, referen
 		if data.LastBattleTime < 1 {
 			continue
 		}
-		if s, ok := existingLastBattleTimes[id]; !force && (ok && data.LastBattleTime == int(s.Unix())) {
+		if s, ok := existingLastBattleTimes[id]; !force && (ok && !s.IsZero() && data.LastBattleTime == int(s.Unix())) {
 			// last snapshot is the same, we can skip it
 			continue
 		}
@@ -155,7 +155,7 @@ func RecordAccountSnapshots(ctx context.Context, wgClient wargaming.Client, dbCl
 		vehicleStats := fetch.WargamingVehiclesToFrame(vehicles.Data)
 		if len(vehicles.Data) > 0 {
 			for id, vehicle := range vehicleStats {
-				if s, ok := existingLastBattleTimes[id]; !force && ok && s.Equal(vehicle.LastBattleTime) {
+				if s, ok := existingLastBattleTimes[id]; !force && ok && !s.IsZero() && s.Equal(vehicle.LastBattleTime) {
 					// last snapshot is the same, we can skip it
 					continue
 				}
