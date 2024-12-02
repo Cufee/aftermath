f41d0ab71966beb594c866a0f6020b8e0059776b
Vovko <vkouzin@outlook.com>
2024-12-01 21:59:42 -0500

    moved migrations

---
+++ internal/database/migrations/20240718195751.sql
@@ -0,0 +1,2 @@
+-- Add column "session_reference_id" to table: "widget_settings"
+ALTER TABLE `widget_settings` ADD COLUMN `session_reference_id` text NULL;
---
+++ internal/database/migrations/20240721174654.sql
@@ -0,0 +1,2 @@
+-- Add column "metadata" to table: "ad_events"
+ALTER TABLE `ad_events` ADD COLUMN `metadata` json NULL;
