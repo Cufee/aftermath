5a43fad4ea268cd5558ea825d39839aebb56b754
Vovko <vkouzin@outlook.com>
2024-12-07 11:51:46 -0500

    fixing migrations

--- internal/database/migrations/20241203184107.sql
+++
@@ -1,6 +0,0 @@
--- Disable the enforcement of foreign-keys constraints
-PRAGMA foreign_keys = off;
--- Rename table "cron_tasks" to "cron_task"
-ALTER TABLE `cron_tasks` RENAME TO `cron_task`;
--- Enable back the enforcement of foreign-keys constraints
-PRAGMA foreign_keys = on;
