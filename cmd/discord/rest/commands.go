e76e760711775309591636e85f2998e7baccca0f
Vovko <vkouzin@outlook.com>
2024-12-29 13:55:14 -0500

    fixed default command context

--- cmd/discord/rest/commands.go
+++ cmd/discord/rest/commands.go
@@ -4,9 +4,10 @@ import (
 	"context"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/cufee/aftermath/internal/discord"
 )
 
-func (c *Client) OverwriteGlobalApplicationCommands(ctx context.Context, data []discordgo.ApplicationCommand) error {
+func (c *Client) OverwriteGlobalApplicationCommands(ctx context.Context, data []discord.ApplicationCommand) error {
 	req, err := c.request("PUT", discordgo.EndpointApplicationGlobalCommands(c.applicationID), data)
 	if err != nil {
 		return err
@@ -14,31 +15,31 @@ func (c *Client) OverwriteGlobalApplicationCommands(ctx context.Context, data []
 	return c.do(ctx, req, nil)
 }
 
-func (c *Client) CreateGlobalApplicationCommand(ctx context.Context, data discordgo.ApplicationCommand) (discordgo.ApplicationCommand, error) {
+func (c *Client) CreateGlobalApplicationCommand(ctx context.Context, data discord.ApplicationCommand) (discord.ApplicationCommand, error) {
 	req, err := c.request("POST", discordgo.EndpointApplicationGlobalCommands(c.applicationID), data)
 	if err != nil {
-		return discordgo.ApplicationCommand{}, err
+		return discord.ApplicationCommand{}, err
 	}
-	var command discordgo.ApplicationCommand
+	var command discord.ApplicationCommand
 	return command, c.do(ctx, req, &command)
 }
 
-func (c *Client) GetGlobalApplicationCommands(ctx context.Context) ([]discordgo.ApplicationCommand, error) {
+func (c *Client) GetGlobalApplicationCommands(ctx context.Context) ([]discord.ApplicationCommand, error) {
 	req, err := c.request("GET", discordgo.EndpointApplicationGlobalCommands(c.applicationID), nil)
 	if err != nil {
 		return nil, err
 	}
 
-	var data []discordgo.ApplicationCommand
+	var data []discord.ApplicationCommand
 	return data, c.do(ctx, req, &data)
 }
 
-func (c *Client) UpdateGlobalApplicationCommand(ctx context.Context, id string, data discordgo.ApplicationCommand) (discordgo.ApplicationCommand, error) {
+func (c *Client) UpdateGlobalApplicationCommand(ctx context.Context, id string, data discord.ApplicationCommand) (discord.ApplicationCommand, error) {
 	req, err := c.request("PATCH", discordgo.EndpointApplicationGlobalCommand(c.applicationID, id), data)
 	if err != nil {
-		return discordgo.ApplicationCommand{}, err
+		return discord.ApplicationCommand{}, err
 	}
-	var command discordgo.ApplicationCommand
+	var command discord.ApplicationCommand
 	return command, c.do(ctx, req, &command)
 }
 
