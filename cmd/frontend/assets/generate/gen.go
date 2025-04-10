a18acd5da378ef8be899f0c5f944dc39bbe4a97e
Vovko <vkouzin@outlook.com>
2025-04-10 08:28:50 -0400

    added missing packages

--- cmd/frontend/assets/generate/gen.go
+++ cmd/frontend/assets/generate/gen.go
@@ -2,6 +2,9 @@ package generate
 
 import (
 	_ "github.com/Kodeworks/golang-image-ico"
+
+	_ "github.com/kolesa-team/go-webp/encoder"
+	_ "github.com/kolesa-team/go-webp/webp"
 )
 
 //go:generate go run ./generate.go
