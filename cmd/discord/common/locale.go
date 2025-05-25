d1881d32509102a662d2e140b31313552f198c98
Vovko <vkouzin@outlook.com>
2025-05-25 12:21:10 -0400

    fixed incorrect localization tags, fixed locale parsing

--- cmd/discord/common/locale.go
+++ cmd/discord/common/locale.go
@@ -28,7 +28,7 @@ func LocaleToLanguageTag(locale discordgo.Locale) language.Tag {
 		return language.English
 
 	default:
-		tag, err := language.Parse(locale.String())
+		tag, err := language.Parse(string(locale))
 		if err != nil {
 			return language.English
 		}
