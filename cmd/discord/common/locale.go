3c6b8ac57674499af5b9258d40b85fd98aff8626
Vovko <vkouzin@outlook.com>
2025-05-25 12:14:56 -0400

    added generic locale tag support

--- cmd/discord/common/locale.go
+++ cmd/discord/common/locale.go
@@ -59,7 +59,12 @@ func LanguageToLocale(tag language.Tag) discordgo.Locale {
 		return discordgo.PortugueseBR
 
 	default:
-		return discordgo.EnglishUS
+		locale := discordgo.Locale(tag.String())
+		_, ok := discordgo.Locales[locale]
+		if !ok {
+			return discordgo.EnglishUS
+		}
+		return locale
 	}
 }
 
