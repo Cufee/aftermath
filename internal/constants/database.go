ae03e6ef59f7a7f2ff675aeabfccb04fee7a6cdc
Vovko <vkouzin@outlook.com>
2025-11-20 22:03:59 -0600

    fixed ssl

--- internal/constants/database.go
+++ internal/constants/database.go
@@ -3,5 +3,5 @@ package constants
 import "fmt"
 
 var (
-	DatabaseConnString = fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=disable", MustGetEnv("DATABASE_USER"), MustGetEnv("DATABASE_PASSWORD"), MustGetEnv("DATABASE_HOST"), MustGetEnv("DATABASE_NAME"))
+	DatabaseConnString = fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=require", MustGetEnv("DATABASE_USER"), MustGetEnv("DATABASE_PASSWORD"), MustGetEnv("DATABASE_HOST"), MustGetEnv("DATABASE_NAME"))
 )
