6e77c2b2d0822b8a28fde95c5016bd29d534f979
Vovko <vkouzin@outlook.com>
2025-11-21 19:39:08 -0600

    update to use database url

--- internal/constants/database.go
+++ internal/constants/database.go
@@ -1,7 +1,5 @@
 package constants
 
-import "fmt"
-
 var (
-	DatabaseConnString = fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=disable", MustGetEnv("DATABASE_USER"), MustGetEnv("DATABASE_PASSWORD"), MustGetEnv("DATABASE_HOST"), MustGetEnv("DATABASE_NAME"))
+	DatabaseConnString = MustGetEnv("DATABASE_URL")
 )
