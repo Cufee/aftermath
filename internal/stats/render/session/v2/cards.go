17540969fa76b0e508aef64049dca9d40cdcbc43
Vovko <vkouzin@outlook.com>
2026-03-21 10:35:37 -0500

    mionor render cleanup

--- internal/stats/render/session/v2/cards.go
+++ internal/stats/render/session/v2/cards.go
@@ -20,28 +20,15 @@ func generateCards(sessionData, careerData fetch.AccountStatsOverPeriod, cards s
 		// primary cards
 		// when there are some unrated battles or no battles at all
 		shouldRenderUnratedOverview = sessionData.RegularBattles.Battles > 0 || sessionData.RatingBattles.Battles < 1
-		// when there are 3 vehicle cards and no rating overview cards or there are 6 vehicle cards and some rating battles
-		shouldRenderUnratedHighlights = (sessionData.RegularBattles.Battles > 0 && sessionData.RatingBattles.Battles < 1 && len(cards.Unrated.Vehicles) > renderUnratedVehiclesCount) ||
-			(sessionData.RegularBattles.Battles > 0 && len(cards.Unrated.Vehicles) > 3)
-		shouldRenderRatingOverview = sessionData.RatingBattles.Battles > 0 && opts.VehicleIDs == nil
-		// secondary cards
-		shouldRenderUnratedVehicles = sessionData.RegularBattles.Battles > 0 && len(cards.Unrated.Vehicles) > 0
+		// more highlights than vehicles implies we will render some vehicles, at least 3 in most cases
+		shouldRenderUnratedHighlights = sessionData.RegularBattles.Battles > 0 && len(cards.Unrated.Vehicles) > len(cards.Unrated.Highlights)
+		// no vehicle stats are available for rating battles
+		shouldRenderRatingOverview = sessionData.RatingBattles.Battles > 0
 	)
-
-	// try to make the columns height roughly similar to primary column
-	if shouldRenderUnratedHighlights {
-		renderUnratedVehiclesCount += len(cards.Unrated.Highlights)
-	}
-	if shouldRenderRatingOverview {
-		renderUnratedVehiclesCount += 1
-	}
-	if len(opts.VehicleIDs) == 1 {
-		renderUnratedVehiclesCount = 0
-	}
 
 	// calculate max overview block width to make all blocks the same size
 	var maxWidthOverviewColumn = make(map[bool]float64)
-	for _, column := range cards.Unrated.Overview.Blocks {
+	for _, column := range append(cards.Unrated.Overview.Blocks, cards.Rating.Overview.Blocks...) {
 		for _, block := range column.Blocks {
 			key := column.Flavor == session.BlockFlavorDefault
 			blockStyle := styledOverviewCard.styleBlock(block)
@@ -49,18 +36,6 @@ func generateCards(sessionData, careerData fetch.AccountStatsOverPeriod, cards s
 			case prepare.TagWN8:
 				block.Label = common.GetWN8TierName(block.Value().Float())
 				maxWidthOverviewColumn[key] = max(maxWidthOverviewColumn[key], iconSizeWN8)
-			}
-			maxWidthOverviewColumn[key] = max(maxWidthOverviewColumn[key],
-				facepaint.MeasureStringWidth(block.Label, blockStyle.label.Font),
-				facepaint.MeasureStringWidth(block.Value().String(), blockStyle.value.Font),
-			)
-		}
-	}
-	for _, column := range cards.Rating.Overview.Blocks {
-		for _, block := range column.Blocks {
-			key := column.Flavor == session.BlockFlavorDefault
-			blockStyle := styledOverviewCard.styleBlock(block)
-			switch block.Tag {
 			case prepare.TagRankedRating:
 				block.Label = common.GetRatingTierName(block.Value().Float())
 				maxWidthOverviewColumn[key] = max(maxWidthOverviewColumn[key], iconSizeRating)
@@ -68,17 +43,6 @@ func generateCards(sessionData, careerData fetch.AccountStatsOverPeriod, cards s
 			maxWidthOverviewColumn[key] = max(maxWidthOverviewColumn[key],
 				facepaint.MeasureStringWidth(block.Label, blockStyle.label.Font),
 				facepaint.MeasureStringWidth(block.Value().String(), blockStyle.value.Font),
-			)
-		}
-	}
-
-	// calculate per block type width of highlight stats to make things even
-	var highlightBlockWidth = make(map[prepare.Tag]float64)
-	for _, highlight := range cards.Unrated.Highlights {
-		for _, block := range highlight.Blocks {
-			highlightBlockWidth[block.Tag] = max(highlightBlockWidth[block.Tag],
-				facepaint.MeasureStringWidth(block.Label, styledHighlightCard.blockLabel().Font),
-				facepaint.MeasureStringWidth(block.Value().String(), styledHighlightCard.blockValue().Font),
 			)
 		}
 	}
@@ -109,6 +73,17 @@ func generateCards(sessionData, careerData fetch.AccountStatsOverPeriod, cards s
 	}
 	// highlights
 	if shouldRenderUnratedHighlights {
+		// calculate per block type width of highlight stats to make things even
+		var highlightBlockWidth = make(map[prepare.Tag]float64)
+		for _, highlight := range cards.Unrated.Highlights {
+			for _, block := range highlight.Blocks {
+				highlightBlockWidth[block.Tag] = max(highlightBlockWidth[block.Tag],
+					facepaint.MeasureStringWidth(block.Label, styledHighlightCard.blockLabel().Font),
+					facepaint.MeasureStringWidth(block.Value().String(), styledHighlightCard.blockValue().Font),
+				)
+			}
+		}
+
 		for _, card := range cards.Unrated.Highlights {
 			overviewCards = append(overviewCards, newHighlightCard(card, highlightBlockWidth))
 		}
@@ -116,13 +91,11 @@ func generateCards(sessionData, careerData fetch.AccountStatsOverPeriod, cards s
 
 	// vehicles
 	var vehicleCards []*facepaint.Block
-	if shouldRenderUnratedVehicles {
-		for i, card := range cards.Unrated.Vehicles {
-			if i == renderUnratedVehiclesCount {
-				break
-			}
-			vehicleCards = append(vehicleCards, newVehicleCard(card, vehicleBlockWidth))
+	for i, card := range cards.Unrated.Vehicles {
+		if i == renderUnratedVehiclesCount {
+			break
 		}
+		vehicleCards = append(vehicleCards, newVehicleCard(card, vehicleBlockWidth))
 	}
 
 	var sectionBlocks []*facepaint.Block
