3cce49b0028f69ffb634c6d30f94d22365205388
Vovko <vkouzin@outlook.com>
2026-03-22 12:05:13 -0500

    cleanup and fix tests

--- internal/external/blitzkit/client.go
+++ internal/external/blitzkit/client.go
@@ -1,23 +1,15 @@
 package blitzkit
 
 import (
-	"context"
 	"net/http"
 	"time"
 
-	"github.com/cufee/aftermath/internal/stats/frame"
 	"github.com/pkg/errors"
 )
 
 //go:generate go tool github.com/bufbuild/buf/cmd/buf generate --template buf.gen.yaml --path averages.proto
 
 var ErrServiceUnavailable = errors.New("blitzkit averages unavailable")
-
-type Client interface {
-	CurrentTankAverages(ctx context.Context) (map[string]frame.StatsFrame, error)
-}
-
-var _ Client = &client{} // just a marker to see if it is implemented correctly
 
 type client struct {
 	http           http.Client
